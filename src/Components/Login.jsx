34b0d96c2817d6172e48049bb14175a171f925f9
Alex <Alexbirchmeyer@gmail.com>
2024-12-28 12:40:38 -0300

    new

---
+++ src/Components/Login.jsx
@@ -0,0 +1,168 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Login = ({ setIsAuthenticated, setRole }) => {
+  // const [user, setUser] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // const [role, setLocalRole] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+
+  
+  const validacion = async () => {
+    if (username && password) {
+      const data = {
+        userName: username,
+        pswHash: password,
+      };
+      try {
+        const res = await fetch("http://localhost:8080/Users/login", {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+        const usuario = await res.json();
+        console.log(usuario);
+        if (usuario) {
+          
+          setMessage("Datos correctos.");
+
+          try {  //prueba de persistencia de cookie
+            await fetch("http://localhost:8080/protected",{
+             method: "GET",
+             credentials:"include"})
+              .then((res) => res.json())
+              .then((data) => {
+                console.log(data);
+              });
+          } catch (error) {
+            console.log(error);
+          }
+          // setIsAuthenticated(true);
+          // switch (usuario.level) {
+          //   case 1:
+          // setRole("Administrador");
+          //     break;
+          //   case 2:
+          //     setRole("Vendedor");
+          //     break;
+          //   case 3:
+          //     setRole("Cliente");
+          //     break;
+          // }
+          // navigate("/dashboard");
+        } else {
+          setMessage("Nombre de usuario o contraseña incorrectos.");
+        }
+      } catch (error) {
+        console.log(error);
+        setMessage("Nombre de usuario o contraseña incorrectos.");
+      }
+    }
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (username === "" || password === "") {
+      setMessage("Por favor, ingrese todos los campos.");
+      return;
+    }
+    validacion();
+  };
+  const handleRegisterClick = () => {
+    navigate("/register"); // Redirigir a la página de registro de clientes
+  };
+
+  return (
+    <div className="container" style={{ maxWidth: "400px", marginTop: "50px" }}>
+      <h2 className="text-center">Login</h2>
+      <form onSubmit={handleLogin}>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">
+            Nombre de Usuario:
+          </label>
+          <input
+            type="text"
+            id="username"
+            className="form-control"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Contraseña:
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="form-control"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary w-100">
+          Iniciar Sesión
+        </button>
+      </form>
+      {message && <p className="text-danger text-center">{message}</p>}
+
+      {/* Botón de Registro */}
+      <div className="text-center mt-3">
+        <p>¿No tienes una cuenta?</p>
+        <button onClick={handleRegisterClick} className="btn btn-secondary">
+          Registrarse
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+//   return (
+//     <div className="container" style={{ maxWidth: "400px", marginTop: "50px" }}>
+//       <h2 className="text-center">Login</h2>
+//       <form onSubmit={handleLogin}>
+//         <div className="mb-3">
+//           <label htmlFor="username" className="form-label">
+//             Nombre de Usuario:
+//           </label>
+//           <input
+//             type="text"
+//             id="username"
+//             className="form-control"
+//             value={username}
+//             onChange={(e) => setUsername(e.target.value)}
+//             required
+//           />
+//         </div>
+//         <div className="mb-3">
+//           <label htmlFor="password" className="form-label">
+//             Contraseña:
+//           </label>
+//           <input
+//             type="password"
+//             id="password"
+//             className="form-control"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//             required
+//           />
+//         </div>
+//         <div className="mb-3"></div>
+//         <button type="submit" className="btn btn-primary w-100">
+//           Iniciar Sesión
+//         </button>
+//       </form>
+//       {message && <p className="text-danger text-center">{message}</p>}
+//     </div>
+//   );
+// };
+
+// export default Login;
---
+++ src/Components/Ventas.jsx
@@ -0,0 +1,532 @@
+import React, { useState, useRef, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Ventas = () => {
+  const [ventas, setVentas] = useState([]);
+  const [formVisible, setFormVisible] = useState(false);
+  const [tableVisible, setTableVisible] = useState(true);
+  const [showRows, setShowRows] = useState(false);
+  const [editingVenta, setEditingVenta] = useState(null);
+  const [rowsSale, setRowsSale] = useState([]);
+  const [productos, setProductos] = useState([]);
+  const [empleados, setEmpleados] = useState([]);
+  const [clientes, setClientes] = useState([]);
+  const [sucursales, setSucursales] = useState([]);
+  const clienteRef = useRef(null);
+  const empleadoRef = useRef(null);
+  const sucursalRef = useRef(null);
+  const totalRef = useRef(null);
+
+  const fetchProd = async () => {
+    try {
+      await fetch("http://localhost:8080/allProducts")
+        .then((res) => res.json())
+        .then((data) => {
+          setProductos(data);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchEmp = async () => {
+      try {
+        await fetch("http://localhost:8080/allEmployees")
+          .then((res) => res.json())
+          .then((data) => {
+            setEmpleados(data);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchClient = async () => {
+      try {
+        await fetch("http://localhost:8080/allClients")
+          .then((res) => res.json())
+          .then((data) => {
+            setClientes(data);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchSuc = async () => {
+      try {
+        await fetch("http://localhost:8080/allBranches")
+          .then((res) => res.json())
+          .then((data) => {
+            setSucursales(data);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    const fetchSale = async () => {
+      try {
+        await fetch("http://localhost:8080/allSales")
+          .then((res) => res.json())
+          .then((data) => {
+            setVentas(data);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchProd();
+    fetchSale();
+    fetchSuc();
+    fetchClient();
+    fetchEmp();
+  }, []);
+
+  const sumarStock = async (id, stock, cantidad) => {
+    try {
+      const datos = {
+        stock: stock + cantidad,
+      };
+      await fetch(`http://localhost:8080/allProducts/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(datos),
+      });
+    } catch (error) {
+      console.log("error al actualiza stock");
+    }
+  };
+
+  const deleteRow = async (id) => {
+    try {
+      const res = await fetch(`http://localhost:8080/allRows/${id}`, {
+        method: "DELETE",
+      });
+    } catch (error) {
+      console.log("error al crear las filas");
+      return;
+    }
+    const updatedRows = rowsSale.filter((r) => r.id !== id);
+    setRowsSale(updatedRows);
+  };
+
+  const creatRows = async (datos) => {
+    try {
+      const res = await fetch(`http://localhost:8080/allRows`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(datos),
+      });
+    } catch (error) {
+      console.log("error al crear las filas");
+      return;
+    }
+  };
+
+  const cargaFilasVenta = async (id) => {
+    try {
+      const res = await fetch(`http://localhost:8080/allRows/${id}`);
+      const data = await res.json();
+      setRowsSale(data);
+      console.log(data);
+      console.log(rowsSale);
+    } catch (error) {
+      console.log("error al actualiza stock");
+    }
+  };
+
+  const updateStock = async (id, stock, cantidad) => {
+    try {
+      const datos = {
+        stock: stock - cantidad,
+      };
+      await fetch(`http://localhost:8080/allProducts/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(datos),
+      });
+    } catch (error) {
+      console.log("error al actualiza stock");
+    }
+  };
+
+  let renglones = [];
+  // Crear venta
+  const createVenta = async (event) => {
+    event.preventDefault();
+    console.log(renglones);
+    toggleFormVisibility();
+    const idClient = clienteRef.current.value;
+    const idEmp = empleadoRef.current.value;
+    const idBranch = sucursalRef.current.value;
+
+    if (idClient && idEmp && idBranch) {
+      let prodFiltrados = [];
+      renglones.forEach((r) => {
+        productos.forEach((prod) => {
+          if (prod.id === r.id) {
+            prodFiltrados.push({ ...prod, cantidad: Number(r.cantidad) });
+          }
+        });
+      });
+      let total = 0;
+      prodFiltrados.map((pr) => (total += pr.price * pr.cantidad));
+      const newSale = {
+        idClient: idClient,
+        idBranch: idBranch,
+        idEmp: idEmp,
+        total: total,
+      };
+      try {
+        const res = await fetch(`http://localhost:8080/allSales`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newSale),
+        });
+        if (res.ok) {
+          const completeSale = await res.json();
+          try {
+            const rows = [];
+            prodFiltrados.forEach((p) => {
+              const newRow = {
+                idSale: completeSale.id,
+                idProduct: p.id,
+                description: p.description,
+                price: p.price,
+                amount: p.cantidad,
+                total: p.cantidad * p.price,
+              };
+              rows.push(newRow);
+            });
+            rows.forEach((row) =>
+              //carga los productos de la venta
+              creatRows(row)
+            );
+
+            prodFiltrados.forEach((p) =>
+              updateStock(p.id, p.stock, p.cantidad)
+            );
+          } catch (error) {
+            console.log(" erro al crear la venta", error);
+          }
+          setVentas([...ventas, completeSale]);
+          resetForm();
+          // setEditingClient(null);
+        }
+      } catch (error) {
+        console.log(" erro al crear la venta", error);
+      }
+    }
+    fetchProd();
+  };
+
+  // Mostrar/ocultar formulario
+  const toggleFormVisibility = () => {
+    setTableVisible(!tableVisible);
+    setFormVisible(!formVisible);
+    setEditingVenta(null); // Resetear venta en edición
+  };
+
+  // Limpiar formulario
+  const resetForm = () => {
+    if (clienteRef.current) clienteRef.current.value = "";
+    if (empleadoRef.current) empleadoRef.current.value = "";
+    if (sucursalRef.current) sucursalRef.current.value = "";
+    if (totalRef.current) totalRef.current.value = "";
+  };
+
+  // Función para actualizar venta
+  const editVenta = (codigo) => {
+    // const venta = ventas.find((v) => v.codigo === codigo);
+    // if (venta) {
+    //   setFormVisible(true);
+    //   setEditingVenta(venta);
+    //   if (codigoRef.current) codigoRef.current.value = venta.codigo;
+    //   if (clienteRef.current)
+    //     clienteRef.current.value =
+    //       clientesEjemplo.find((cliente) => cliente.nombre === venta.cliente)
+    //         ?.id || "";
+    //   if (empleadoRef.current)
+    //     empleadoRef.current.value =
+    //       empleadosEjemplo.find(
+    //         (empleado) => empleado.nombre === venta.empleado
+    //       )?.id || "";
+    //   if (regionRef.current) regionRef.current.value = venta.region;
+    //   if (totalRef.current) totalRef.current.value = venta.total;
+    // }
+  };
+
+  // Función para eliminar venta
+  const deleteVenta = async (id) => {
+    const confirmDelete = window.confirm(
+      "¿Estás seguro de eliminar esta venta?"
+    );
+    if (confirmDelete) {
+      await fetch(`http://localhost:8080/allSales/${id}`, {
+        method: "DELETE",
+      });
+      await fetch(`http://localhost:8080/allRows/sale/${id}`, {
+        method: "DELETE",
+      });
+      const updatedVentas = ventas.filter((v) => v.id !== id);
+      cargaFilasVenta(id);
+      rowsSale.forEach((r) => sumarStock(r.idProduct, r.amount));
+
+      setVentas(updatedVentas);
+      setRowsSale(null);
+      setShowRows(false);
+    }
+  };
+
+  const CheckboxChange = (idProducto) => {
+    const estaSeleccionado = renglones.find((prod) => prod.id === idProducto);
+    if (estaSeleccionado) {
+      // Si ya está en el array, quítalo (desmarcar checkbox)
+      renglones = renglones.filter((prod) => prod.id !== idProducto);
+    } else {
+      // Si no está en el array, agrégalo con cantidad inicial de 1
+      renglones.push({ id: idProducto, cantidad: 1 });
+    }
+  };
+
+  const CantidadChange = (idProducto, cantidad) => {
+    const estaSeleccionado = renglones.find((prod) => prod.id === idProducto);
+    if (estaSeleccionado) {
+      // Si ya está en el array, quítalo (desmarcar checkbox)
+      const index = renglones.findIndex((prod) => prod.id == idProducto);
+      renglones[index].cantidad = Number(cantidad);
+    }
+  };
+
+  return (
+    <div style={{ marginTop: "5%" }}>
+      <div className="btn-group" style={{ marginBottom: "3%" }}>
+        <button
+          id="b_create"
+          onClick={toggleFormVisibility}
+          type="button"
+          className="btn btn-primary"
+        >
+          {formVisible ? "Cancelar" : "Crear Venta"}
+        </button>
+      </div>
+
+      {/* Formulario visible para crear o editar venta */}
+      {formVisible && (
+        <form id="ventaForm" onSubmit={createVenta} style={{ marginTop: "5%" }}>
+          <div className="mb-3">
+            <label htmlFor="Cliente" className="form-label">
+              Cliente:
+            </label>
+            <select
+              ref={clienteRef}
+              name="Cliente"
+              className="form-control"
+              required
+            >
+              <option value="">Seleccione un cliente</option>
+              {clientes.map((cliente) => (
+                <option key={cliente.id} value={cliente.id}>
+                  {cliente.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="Empleado" className="form-label">
+              Empleado:
+            </label>
+            <select
+              ref={empleadoRef}
+              name="Empleado"
+              className="form-control"
+              required
+            >
+              <option value="">Seleccione un empleado</option>
+              {empleados.map((empleado) => (
+                <option key={empleado.id} value={empleado.id}>
+                  {empleado.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="sucursal" className="form-label">
+              Sucursal:
+            </label>
+            <select
+              ref={sucursalRef}
+              name="sucursal"
+              className="form-control"
+              required
+            >
+              <option value="">Seleccione una Sucursal</option>
+              {sucursales.map((suc) => (
+                <option key={suc.id} value={suc.id}>
+                  {suc.id}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="Productos" className="form-label">
+              Productos Disponibles:
+            </label>
+            <table className="table table-bordered" id="ventaTable">
+              <thead className="table-dark">
+                <tr>
+                  <th></th>
+                  <th>Cantidad</th>
+                  <th>Descripcion</th>
+                  <th>Precio</th>
+                  <th>Stock</th>
+                </tr>
+              </thead>
+              <tbody>
+                {productos.map((producto) => (
+                  <tr key={producto.id}>
+                    <td style={{ width: "100px" }}>
+                      <input
+                        type="checkbox"
+                        className="btn-check"
+                        id={`btncheck-${producto.id}`}
+                        onChange={() => CheckboxChange(producto.id)}
+                      />
+                      <label
+                        className="btn btn-outline-primary"
+                        htmlFor={`btncheck-${producto.id}`}
+                      >
+                        Seleccionar
+                      </label>
+                    </td>
+                    <td style={{ width: "100px" }}>
+                      <input
+                        type="number"
+                        name="cantidad"
+                        className="form-control"
+                        onChange={(e) =>
+                          CantidadChange(producto.id, e.target.value)
+                        }
+                        min="1"
+                      />
+                    </td>
+                    <td>{producto.description}</td>
+                    <td>{producto.price}</td>
+                    <td>{producto.stock}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            {editingVenta ? "Actualizar Venta" : "Guardar Venta"}
+          </button>
+        </form>
+      )}
+
+      {tableVisible && (
+        <div className="table-responsive">
+          <h2>Listado de Ventas</h2>
+          <table className="table table-bordered" id="ventaTable">
+            <thead className="table-dark">
+              <tr>
+                <th>ID</th>
+                <th>Cliente</th>
+                <th>Empleado</th>
+                <th>Sucursal</th>
+                <th>Fecha</th>
+                <th>Total</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {ventas.length > 0 ? (
+                ventas.map((venta) => (
+                  <tr key={venta.id}>
+                    <td>{venta.id}</td>
+                    <td>{venta.idClient}</td>
+                    <td>{venta.idEmp}</td>
+                    <td>{venta.idBranch}</td>
+                    <td>{venta.createdAt}</td>
+                    <td>${venta.total}</td>
+                    <td>
+                      <button
+                        onClick={() => editVenta(venta.codigo)}
+                        className="btn btn-warning btn-sm me-2"
+                      >
+                        Actualizar
+                      </button>
+                      <button
+                        onClick={() => deleteVenta(venta.id)}
+                        className="btn btn-danger btn-sm me-2"
+                      >
+                        Eliminar
+                      </button>
+                      <button
+                        onClick={() => {
+                          cargaFilasVenta(venta.id);
+                          setShowRows(!showRows);
+                        }}
+                        className="btn btn-info btn-sm "
+                      >
+                        {showRows ? "Cerrar" : "Abrir"}
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan={7}>No hay ventas registradas</td>
+                </tr>
+              )}
+              {showRows && (
+                <table className="table table-bordered" id="ventaTable">
+                  <thead className="table-dark">
+                    <tr>
+                      <th>Producto</th>
+                      <th>Precio</th>
+                      <th>Cantidad</th>
+                      <th>Total</th>
+                      <th>Acciones</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {rowsSale.map((row) => (
+                      <tr key={row.id}>
+                        <td>{row.description}</td>
+                        <td>{row.price}</td>
+                        <td>{row.amount}</td>
+                        <td>{row.total}</td>
+                        <td>
+                          <button
+                            className="btn btn-danger btn-sm me-2"
+                            onClick={() => deleteRow(row.id)}
+                          >
+                            Eliminar
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              )}
+            </tbody>
+          </table>
+        </div>
+      )}
+      {/* Tabla de Ventas */}
+    </div>
+  );
+};
+
+export default Ventas;
