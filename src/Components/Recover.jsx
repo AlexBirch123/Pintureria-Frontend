5c5bb798a70ae6944d1a7f4942178d05bb58f18f
Alex <Alexbirchmeyer@gmail.com>
2025-02-12 12:42:18 -0300

    recuperacion de contraseña de usuario

--- src/Components/Recover.jsx
+++ src/Components/Recover.jsx
@@ -1,78 +1,127 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "./AuthContext.jsx";
 
 const Recover = () => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const queryToken = queryParams.get('token');
   const [email, setEmail] = useState("");
+  const [token, setToken] = useState(null);
+  const [passChanged,setPassChanged] = useState(false);
   // const [username, setUsername] = useState("");
-  // const [password, setPassword] = useState("");
+  const [pass, setPass] = useState("");
+  const [pass2, setPass2] = useState("");
   // // const [role, setLocalRole] = useState("");
   const [message, setMessage] = useState("");
   // const {setIsAuthenticated,setRole } = useAuth()
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  // const validacion = async () => {
-  //   if (username && password) {
-  //     const data = {
-  //       userName: username,
-  //       pswHash: password,
-  //     };
-  //     try {
-  //       const res = await fetch( process.env.URL +"/users/login", {
-  //         method: "POST",
-  //         credentials: "include",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //         body: JSON.stringify(data),
-  //       });
-  //       if (!res.ok) {
-  //         return setMessage("Credenciales incorrectas");
 
-  //       }
-  //       const level = await res.json()
+  useEffect(() => {
+    const fetchToken = async () => {
+      try {
+        const res = await fetch(`http://localhost:8080/recover/${queryToken}`, {
+          credentials: "include",
+        });
+        const data = await res.json();
+        if (data) setEmail(data.email);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (queryToken) {
+      setToken(queryToken);
+      fetchToken();
+    }
+  }, [queryToken]);
 
-  //       setMessage("Datos correctos");
-  //       setIsAuthenticated(true);
-  //       setRole(level)
-  //       navigate("/dashboard");
-
-  //     } catch (error) {
-  //       setMessage("Nombre de usuario o contraseña incorrectos");
-  //     }
-  //   }
-  // };
-
-  const handleRecover = async (e) => {
+  const changePass = async (e) => {
     e.preventDefault();
+    if (!(pass === pass2)) return setMessage("Las contraseñas no coinciden");
+    const data = {
+      pswHash: pass,
+      email: email,
+    };
     try {
-      await fetch(process.env.REACT_APP_API_URL + "/recover", {
-        method:"POST",
+      const res = await fetch(process.env.REACT_APP_API_URL + `/users`, {
+        method: "PATCH",
         credentials: "include",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(),
-      })
-
+        body: JSON.stringify(data),
+      });
+      if (res.ok) {
+        setMessage("Contraseña modificada con exito, redirigiendo al login");
+        setTimeout(() => navigate("/login"), 3000);
+      }
     } catch (error) {
-      console.log(error)
+      setMessage("Error en la solicitud");
     }
-    //   if (username === "" || password === "") {
-    //     setMessage("Por favor, ingrese todos los campos.");
-    //     return;
-    //   }
-    //   validacion();
-    // };
-    // const handleRegisterClick = () => {
-    //   navigate("/register"); // Redirigir a la página de registro de clientes
+  };
+
+  const handleRecover = async (e) => {
+    e.preventDefault();
+    try {
+        const res = await fetch("http://localhost:8080/recover", {
+          method:"POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(email),
+        })
+        if(res.ok) {
+          setMessage(`Se envio un correo de recuperacion a ${email}`)
+          setEmail(null)
+        }else{
+          setMessage("El correo electronico ingresado no esta ligado a un usuario existente")
+          setTimeout(()=>setMessage(null),5000)
+        }
+    } catch (error) {
+      setMessage(`Error al enviar correo de recuperacion a ${email}`)
+      setTimeout(()=>setMessage(null),3000)
+    }
   };
 
   return (
+    <div>
+    {token && email ? (
+      <div className="container" style={{ maxWidth: "400px", marginTop: "200px" }}>
+      <h2 className="text-center">Recuperacion de contraseña</h2>
+      <form onSubmit={changePass}>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">
+            Nueva contraseña:
+          </label>
+          <input
+            type="password"
+            id="pass"
+            className="form-control"
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+          />
+          <label htmlFor="username" className="form-label">
+            Repita contraseña:
+          </label>
+          <input
+            type="password"
+            id="pass2"
+            className="form-control"
+            value={pass2}
+            onChange={(e) => setPass2(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary w-100">
+          Enviar
+        </button>
+      </form>
+      {message && <p className="text-danger text-center">{message}</p>}
+    </div>
+    ):(
     <div className="container" style={{ maxWidth: "400px", marginTop: "200px" }}>
-      
-      <button onClick={handleRecover}>Enviar mail</button>
-      {/* <h2 className="text-center">Recuperacion de contraseña</h2>
+      <h2 className="text-center">Recuperacion de contraseña</h2>
       <form onSubmit={handleRecover}>
         <div className="mb-3">
           <label htmlFor="username" className="form-label">
@@ -87,23 +136,13 @@ const Recover = () => {
           />
         </div>
         <button type="submit" className="btn btn-primary w-100">
-          Confirmar
+          Enviar
         </button>
       </form>
-      {message && <p className="text-danger text-center">{message}</p>} */}
-
-      {/* Botón de Registro */}
-      {/* <div className="text-center mt-3">
-        <Link className="link" to="/recoverPassword">
-          ¿Olvidó su contraseña?
-        </Link>
-        <p>¿No tienes una cuenta?</p>
-        <button onClick={handleRegisterClick} className="btn btn-secondary">
-          Registrarse
-        </button>
-      </div> */}
+      {message && <p className="text-danger text-center">{message}</p>}
+    </div>)}
     </div>
-  );
+);
 };
 
 export default Recover;
