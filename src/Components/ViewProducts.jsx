55fafc1a1af7eab288a70bd0c2faab5a5bf3f549
Alex <alexbichmeyer@gmail.com>
2025-04-24 15:41:04 -0300

    optimizacion de busqueda de productos

--- src/Components/ViewProducts.jsx
+++ src/Components/ViewProducts.jsx
@@ -8,76 +8,13 @@ const ViewProducts = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const category = queryParams.get('category');
-  const description = queryParams.get('description');
+  const title = queryParams.get('title');
   const [filteredProds,setFilteredProds]= useState([])
   const [cat, setCat] = useState(category);
   const [products, setProducts] = useState([]);
   const [max, setMax] = useState(0);
   const [min, setMin] = useState(0);
   const [categories, setCategories] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async (url, localStorageKey, setState) => {
-          const local = getLocalStorage(localStorageKey);
-          try {
-            const res = await fetch(process.env.REACT_APP_API_URL + url, {
-              credentials: "include",
-            });
-            if (!res.ok) return setState(local.datos);
-            const data = await res.json();
-            setState(data);
-            setLocalStorage(data, localStorageKey);
-          } catch (error) {
-            setState(local.datos);
-          }
-        };
-
-    fetchData("/products", "products", setProducts);
-    fetchData("/category", "category", setCategories);
-  }, []);
-
-  useEffect(() => {
-    setCat(category)
-    handleFilteredProds();
-  }, [category, products, description]);
-
-  const handleFilteredProds = () => {
-    if(!products) return setFilteredProds([])
-      else{
-    if (cat) {
-      const prods = products.filter((p) => p.idCat === Number(cat));
-      setFilteredProds(prods);
-      setMaxMin(prods);
-      return
-    }
-    if (description) {
-      const prods = products.filter((p) => p.description.toLowerCase().includes(description.toLowerCase()));
-      setFilteredProds(prods);
-      setMaxMin(prods);
-      return
-    }
-    setFilteredProds(products);
-    setMaxMin(products);
-      }
-  };
-
-  const setMaxMin = (prods) => {
-    if (!Array.isArray(prods) ||prods.length === 0) {
-      setMax(0)
-      setMin(0)
-      return
-    };
-    setMax(Math.max(...prods.map(p => p.price)));
-    setMin(Math.min(...prods.map(p => p.price)));
-  };
-
-  // const searchCat = () => {
-  //   if (!cat) return false;
-  //   const categoria = categories.find((p) => p.id === cat);
-  //   return categoria.description;
-  // };
-
-  const [showFilters, setShowFilters] = useState(window.innerWidth > 768);
 
   useEffect(() => {
     const handleResize = () => {
@@ -90,6 +27,63 @@ const ViewProducts = () => {
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
+
+  useEffect(() => {
+    fetchData("/category", "category", setCategories);
+    setCat(category)
+  }, []);
+
+  useEffect(() => {
+    handleFilteredProds();
+  }, [cat,category, products, title]);
+  
+
+  const fetchData = async (url, localStorageKey, setState) => {
+    const local = getLocalStorage(localStorageKey);
+    try {
+      const res = await fetch(process.env.REACT_APP_API_URL + url, {
+        credentials: "include",
+      });
+      if (!res.ok) return setState(local.datos);
+      const data = await res.json();
+      setState(data);
+      setLocalStorage(data, localStorageKey);
+      return data;
+    } catch (error) {
+      setState(local.datos);
+    }
+  };
+
+  const handleFilteredProds = async() => {
+    if (cat) {
+      const data = await fetchData(`/products/category/${cat}`, "products", setFilteredProds);
+      setMaxMin(data);
+      return
+    }
+    if (title) {
+      const data = await fetchData(`/products/title/${title}`, "products", setFilteredProds);
+      setMaxMin(data);
+      return
+    }
+    const data = await fetchData("/products", "products", setFilteredProds);
+    console.log(data)
+    setMaxMin(data);
+    
+  };
+
+  const setMaxMin = (prods) => {
+    if (!Array.isArray(prods) ||prods.length === 0) {
+      setMax(0)
+      setMin(0)
+      return
+    };
+    setMax(Math.max(...prods.map(p => Number(p.price))));
+    setMin(Math.min(...prods.map(p => Number(p.price))));
+  };
+
+
+  const [showFilters, setShowFilters] = useState(window.innerWidth > 768);
+
 
   return (
     <div className="container mt-5">
@@ -141,7 +135,7 @@ const ViewProducts = () => {
           <div className="card p-3 shadow-sm h-100">
             <h2 className="text-center">Productos</h2>
             {cat && <p className="text-muted">Mostrando products para la categoría: {cat}</p>}
-            {description && <p className="text-muted">Mostrando products para su búsqueda: {description}</p>}
+            {title && <p className="text-muted">Mostrando products para su búsqueda: {title}</p>}
             <div className="row g-3 mt-3">
               {filteredProds.length > 0 ? (
                 filteredProds.map((p) => (
